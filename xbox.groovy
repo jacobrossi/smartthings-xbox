d9db9aff611aec1d0a69a7a3b53939d88895e596
jacobrossi <gtjrossi@gmail.com>
2017-03-18 12:45:25 -0700

    Move to multi attr tile and standardize whitespace

--- xbox.groovy
+++ xbox.groovy
@@ -7,35 +7,35 @@
  */
 
 preferences {
-	input("serverIp", "text", title: "Bridge IP", description: "The IP of the bridge (server) running the node module")
-	input("serverPort", "number", title: "Bridge Port", description: "The port of the smartthings-xbox bridge (default: 80)")
-	input("xboxIp", "text", title: "Xbox IP", description: "The IP of the Xbox to control")
-	input("xboxDeviceId", "text", title: "Xbox Live Device ID", description: "The Xbox Live Device ID for your Xbox (found in Settings > System)")
+    input("serverIp", "text", title: "Bridge IP", description: "The IP of the bridge (server) running the node module")
+    input("serverPort", "number", title: "Bridge Port", description: "The port of the smartthings-xbox bridge (default: 80)")
+    input("xboxIp", "text", title: "Xbox IP", description: "The IP of the Xbox to control")
+    input("xboxDeviceId", "text", title: "Xbox Live Device ID", description: "The Xbox Live Device ID for your Xbox (found in Settings > System)")
 }
- 
 
 metadata {
-	definition (name: "Xbox One Controller", namespace: "JacobRossi", author: "gtjrossi@gmail.com") {
+    definition (name: "Xbox One Controller", namespace: "JacobRossi", author: "gtjrossi@gmail.com") {
         capability "Actuator"
         capability "Switch" 
         capability "Configuration"
-      	}
+    }
 
-	tiles {
-		standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
-			state "off", label: '${name}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
-			state "on", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821"
-		}
+    tiles (scale: 2) {
+        multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true) {
+            tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
+                attributeState "on", label:'${name}', action:"switch.off", icon:"st.Electronics.electronics5", backgroundColor:"#79b821"
+                attributeState "off", label:'${name}', action:"switch.on", icon:"st.Electronics.electronics5", backgroundColor:"#ffffff"
+            }
+        }
 
-		main(["switch"])
-		details(["switch"])
-	}
+        main(["switch"])
+        details(["switch"])
+    }
 }
 
 def parse(String description) {
-	log.debug "Parsing '${description}'"
+    log.debug "Parsing '${description}'"
 }
-
 
 def on() {
     sendEvent(name: "switch", value: "on")
